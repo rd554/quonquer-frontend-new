7562d16acecbe1f5a32c4eac57de671c681075b1
Ravi <quonquer@gmail.com>
2020-10-19 06:16:27 +0530

    changed ip in config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
 
 export const API = publicRuntimeConfig.PRODUCTION
-  ? "https://quonquer.com"
+  ? "http://13.233.86.221"
   : "http://192.168.0.109:8000";
 
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
